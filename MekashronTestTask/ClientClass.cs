9c2f73cc0621998f0f55f28c7f3f1e5c8540190e
Nikolay Kozhevnikov <nkozhevn2000@gmail.com>
2023-10-25 14:26:30 +0400

    Project set up

---
+++ MekashronTestTask/ClientClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace MekashronTestTask
+{
+    public class ClientClass
+    {
+        private readonly string serviceUrl;
+
+        public ClientClass(string serviceUrl)
+        {
+            this.serviceUrl = serviceUrl;
+        }
+
+        public async Task<string> CallWebServiceMethodAsync(string methodName, string login, string password)
+        {
+            string iPs = "0";
+            string soapRequest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
+            <soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+                <soap:Body>
+                    <{methodName} xmlns=""urn:ICUTech.Intf-IICUTech"">
+                        <UserName>{login}</UserName>
+                        <Password>{password}</Password>
+                        <IPs>{iPs}</IPs>
+                    </{methodName}>
+                </soap:Body>
+            </soap:Envelope>";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(soapRequest, Encoding.UTF8, "text/xml");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                var response = await client.PostAsync(serviceUrl, content);
+                var soapResponse = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(soapResponse);
+
+                XDocument xdoc = XDocument.Parse(soapResponse);
+
+                XElement returnElement = xdoc.Descendants("return").FirstOrDefault();
+                if (returnElement != null)
+                {
+                    string jsonValue = returnElement.Value;
+                    Console.WriteLine(jsonValue);
+
+                    var json = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonValue);
+
+                    if(json.ContainsKey("ResultCode"))
+                    {
+                        int resultCode = json.ResultCode;
+                        if(resultCode == -1)
+                        {
+                            return resultCode.ToString();
+                        }
+                    }
+                    if(json.ContainsKey("EntityId"))
+                    {
+                        string entityId = json.EntityId;
+                        return entityId;
+                    }
+                    else
+                    {
+                        return "-1";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Элемент <return> не найден в ответе.");
+                    return "-1";
+                }
+            }
+        }
+    }
+}
+
---
+++ MekashronTestTask/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace MekashronTestTask
+{
+    public class LoginController : SurfaceController
+    {
+        public LoginController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult CallWebService([FromQuery] string login, string password)
+        {
+            ClientClass client = new ClientClass("http://isapi.icu-tech.com/icutech-test.dll/soap/IICUTech");
+
+            string result = client.CallWebServiceMethodAsync("Login", login, password).Result;
+
+            return Content(result);
+        }
+    }
+}
+
